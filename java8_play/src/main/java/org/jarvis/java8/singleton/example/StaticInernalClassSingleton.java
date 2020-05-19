e18534b516ae1a500e3aa57a50a35c254c175960
tennyson <lovelytrump@outlook.com>
2020-05-19 23:35:46 +0800

    静态内部类实现单例和懒加载:yum:

--- java8_play/src/main/java/org/jarvis/java8/singleton/example/StaticInernalClassSingleton.java
+++ java8_play/src/main/java/org/jarvis/java8/singleton/example/StaticInernalClassSingleton.java
@@ -7,17 +7,20 @@ package org.jarvis.java8.singleton.example;
  * 原文链接：https://blog.csdn.net/qq_44543508/article/details/103249751
  * author:Lovel  date:2020/5/19
  */
-public class StaticIntalClassSingleton {
-    private StaticIntalClassSingleton() {
+public class StaticInernalClassSingleton {
+    private StaticInernalClassSingleton() {
     }
 
-    //写一个static静态内部类，给该类添加一个static静态instance属性
+    /*写一个static静态内部类，给该类添加一个static静态instance属性
+    内部类不会在其外部类被加载的同时被加载的事实，我们可以引申出单例模式的一种实现方式： 静态内部类*/
     private static class SingletonInstance {
-        private static final StaticIntalClassSingleton INTAL_CLASS_SINGLETON = new StaticIntalClassSingleton();
+        private static final StaticInernalClassSingleton INTAL_CLASS_SINGLETON = new StaticInernalClassSingleton();
     }
-
-    //
-    public static synchronized StaticIntalClassSingleton getInstence() {
+    /**
+     * 调用构造方法时，外部类Outer被加载，但这时其静态内部类StaticInner却未被加载。
+     * 直到调用该内部类的静态方法才被加载。
+     */
+    public static synchronized StaticInernalClassSingleton getInstence() {
         return SingletonInstance.INTAL_CLASS_SINGLETON;
     }
 }
